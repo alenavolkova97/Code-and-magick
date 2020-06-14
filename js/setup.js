dd72aa517b8d87bb09f69081ae0970560cf1b3c7
alenashashko <L.Shashko@mail.ru>
2020-06-15 00:11:45 +0300

    Реализует валидацию ввода имени персонажа и изменение цветов элементов персонажа

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 // переменные
 
@@ -18,7 +19,17 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 var similarWizardsList = document.querySelector('.setup-similar-list');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
-var setupUserName = userDialog.querySelector('.setup-user-name');
+var userNameInput = userDialog.querySelector('.setup-user-name');
+var form = document.querySelector('.setup-wizard-form');
+var wizardCoatElement = document.querySelector('#wizard-coat');
+var wizardCoatColorInput = document.querySelector('input[name="coat-color"]');
+var wizardEyesElement = document.querySelector('#wizard-eyes');
+var wizardEyesColorInput = document.querySelector('input[name="eyes-color"]');
+var wizardFireballWrap = document.querySelector('.setup-fireball-wrap');
+var wizardFireballColorInput = document.querySelector('input[name="fireball-color"]');
+
+var MIN_NAME_LENGTH = userNameInput.minLength;
+var MAX_NAME_LENGTH = userNameInput.maxLength;
 
 // объявления функций
 
@@ -92,7 +103,7 @@ var closeUserDialog = function () {
 // объявления обработчиков
 
 var userDialogEscPressHandler = function (evt) {
-  if (evt.key === 'Escape' && document.activeElement !== setupUserName) {
+  if (evt.key === 'Escape' && document.activeElement !== userNameInput) {
     evt.preventDefault();
     userDialog.classList.add('hidden');
   }
@@ -101,3 +112,41 @@ var userDialogEscPressHandler = function (evt) {
 // вызовы функций
 
 init();
+
+userNameInput.addEventListener('invalid', function () { // сделать
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () { // сделать
+  var valueLength = userNameInput.value.length;
+  if (!form.reportValidity() && valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCastomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (!form.reportValidity() && valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCastomValidity(' Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+wizardCoatElement.addEventListener('click', function () { // не работает автосмена и не работает отправка на сервер
+  wizardCoatElement.style.fill = WIZARD_COAT_COLORS[getRandomNumber(WIZARD_COAT_COLORS.length - 1)];
+  wizardCoatColorInput.value = wizardCoatElement.style.fill;
+});
+
+wizardEyesElement.addEventListener('click', function () { // не работает автосмена и не работает отправка на сервер
+  wizardEyesElement.style.fill = WIZARD_EYES_COLORS[getRandomNumber(WIZARD_EYES_COLORS.length - 1)];
+  wizardEyesColorInput.value = wizardEyesElement.style.fill;
+});
+
+wizardFireballWrap.addEventListener('click', function () { // не работало при записи в переменную
+  wizardFireballWrap.style.backgroundColor = WIZARD_FIREBALL_COLORS[getRandomNumber(WIZARD_FIREBALL_COLORS.length - 1)];
+  wizardFireballColorInput.value = wizardFireballWrap.style.backgroundColor;
+});
